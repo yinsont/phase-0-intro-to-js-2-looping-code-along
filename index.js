330589f5cf875d1df8f3b8d2e7dd12dda3d4a098
Yinson Tso <yinson.tso@gmail.com>
2023-03-08 16:09:40 -0500

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arr, str){
+    let newArr = []
+    for (let i of arr){
+        newArr.push(`Thank you, ${i}, for the wonderful ${str} gift!`)
+    }
+    return newArr
+}
+
+function countDown(n){
+    while (n >= 0){
+        console.log(n)
+        n -= 1
+    }
+}
